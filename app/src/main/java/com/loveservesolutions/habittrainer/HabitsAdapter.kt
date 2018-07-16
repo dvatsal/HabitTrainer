5b3bfe93d2f1bb046a90716cab6188c2dff54e1f
Vatsal Desai <vdesai@mvixusa.com>
2018-07-16 18:54:00 +0530

    method implementation

--- app/src/main/java/com/loveservesolutions/habittrainer/HabitsAdapter.kt
+++ app/src/main/java/com/loveservesolutions/habittrainer/HabitsAdapter.kt
@@ -1,6 +1,7 @@
 package com.loveservesolutions.habittrainer
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
@@ -8,13 +9,13 @@ class HabitsAdapter(val habits:List<Habit>):RecyclerView.Adapter<HabitsAdapter.H
 
     class HabitViewHolder(val iv:View) : RecyclerView.ViewHolder(iv)
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): HabitViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    //Create new view holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_card, parent)
+        return HabitViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount() = habits.size
 
     override fun onBindViewHolder(holder: HabitViewHolder?, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
