4efcb8fa42ccb159215c2ea0742f6e211085654e
dvatsal <vatsaldin@gmail.com>
2018-07-16 22:08:51 +0530

    kotlin version changed in gradle file elevation added padding and margin added

--- app/src/main/java/com/loveservesolutions/habittrainer/HabitsAdapter.kt
+++ app/src/main/java/com/loveservesolutions/habittrainer/HabitsAdapter.kt
@@ -4,10 +4,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.single_card.view.*
 
 class HabitsAdapter(val habits:List<Habit>):RecyclerView.Adapter<HabitsAdapter.HabitViewHolder>(){
 
-    class HabitViewHolder(val iv:View) : RecyclerView.ViewHolder(iv)
+    class HabitViewHolder(val card:View) : RecyclerView.ViewHolder(card)
 
     //Create new view holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
@@ -17,7 +18,14 @@ class HabitsAdapter(val habits:List<Habit>):RecyclerView.Adapter<HabitsAdapter.H
 
     override fun getItemCount() = habits.size
 
-    override fun onBindViewHolder(holder: HabitViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    //Specifies content to be shown
+    override fun onBindViewHolder(holder: HabitViewHolder?, index: Int) {
+        if(holder!=null)
+        {
+            holder.card.tv_title.text = habits[index].title
+            holder.card.tv_description.text = habits[index].description
+            holder.card.iv_icon.setImageResource(habits[index].image)
+
+        }
     }
 }
