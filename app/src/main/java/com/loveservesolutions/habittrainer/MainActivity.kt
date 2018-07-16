30e5aa74d7bac76768898fa25dc1c04ae51d30b8
dvatsal <vatsaldin@gmail.com>
2018-07-16 10:22:23 +0530

    change over weekend

--- app/src/main/java/com/loveservesolutions/habittrainer/MainActivity.kt
+++ app/src/main/java/com/loveservesolutions/habittrainer/MainActivity.kt
@@ -2,11 +2,17 @@ package com.loveservesolutions.habittrainer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+
+        //adapter - defines data
+        //Recycler view implements 3 methods
+
+            }
 }
