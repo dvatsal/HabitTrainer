47bbaeede3f60fc220eed19e3883c836cdeeba34
Vatsal Desai <vdesai@mvixusa.com>
2018-07-18 18:40:07 +0530

    button action to add habit

--- app/src/main/java/com/loveservesolutions/habittrainer/CreateHabitActivity.kt
+++ app/src/main/java/com/loveservesolutions/habittrainer/CreateHabitActivity.kt
@@ -1,9 +1,11 @@
 package com.loveservesolutions.habittrainer
 
 import android.content.Intent
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 
 class CreateHabitActivity : AppCompatActivity() {
 
@@ -23,5 +25,13 @@ class CreateHabitActivity : AppCompatActivity() {
     private fun switchActivity(c:Class<*>) {
         val intent = Intent(this, c)
         startActivity(intent)
+    }
+
+    fun chooseImage(v:View){
+        val intent = Intent()
+        intent.type ="image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        val chooser = Intent.createChooser(intent, "Choose image for habit")
+        startActivity(chooser)
     }
 }
